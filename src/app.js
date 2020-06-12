a6977d33d63af329eb1f9ca1fff4494da575fd98
vedanshu007 <sharmavedanshu@gmail.com>
2020-06-12 14:28:25 +0530

    heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geoloc = require('./utils/geoloc')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 
 const publicDir = path.join(__dirname, '../public')
@@ -80,6 +81,6 @@ app.get('*', (req, res) =>{
 
 
 
-app.listen(3000, () =>{
-    console.log('listening to port 3000')
+app.listen(port, () =>{
+    console.log('listening to port' + port)
 })
